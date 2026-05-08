ea8baf7724c927dedb103f97a82649edd18747c2
Sinval Luz <contato.sinvalluz@gmail.com>
2026-05-08 09:43:30 -0300

    fix: adiciona as urls válidas na cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.enableCors({
-		origin: ['http://localhost:5173/', env.APP_URL],
+		origin: [
+			'http://localhost:5173/',
+			'https://modelo-engenharia-software.vercel.app',
+			'https://modelo-engenharia-software-nzm5.vercel.app',
+		],
 		credentials: true,
 	});
 
