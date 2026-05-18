50ca27a6ae7f029f3a96207e7ace667b311ff08b
Sinval Luz <contato.sinvalluz@gmail.com>
2026-05-18 12:04:56 -0300

    feat: adiciona cards de lançamentos vazios, carregando e erro

--- client/src/app/routes/app/Launches.tsx
+++ client/src/app/routes/app/Launches.tsx
@@ -1,21 +1,21 @@
-import { Plus } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import NewLaunchButton from '@/components/NewLaunchButton';
+import LaunchesEmptyCards from '@/features/launches/components/EmptyLaunchesCard';
+import ErrorLaunchesCard from '@/features/launches/components/ErrorLaunchesCard';
 import FilterCard from '@/features/launches/components/FilterCard';
+import LoadingLaunchesCard from '@/features/launches/components/LoadingLaunchesCard';
 
 export default function LaunchesRoute() {
 	return (
-		<div className='p-10 flex-1'>
+		<div className='p-10 flex flex-col flex-1 space-y-4'>
 			<div className='flex flex-col items-start md:flex-row md:items-center md:justify-between mb-3'>
 				<div>
 					<h2 className='text-foreground font-bold text-xl'>Lançamentos</h2>
 					<span className='text-sm text-secondary-foreground'>20 Registros encontrados</span>
 				</div>
-				<Button className='cursor-pointer bg-[#1f7a6b] hover:bg-[#2fae8f] text-white h-12 px-6 flex items-center mt-3'>
-					<Plus className='size-5' />
-					<span className='leading-none'>Novo Lançamento</span>
-				</Button>
+				<NewLaunchButton />
 			</div>
 			<FilterCard />
+			<LaunchesEmptyCards />
 		</div>
 	);
 }
---
+++ client/src/features/launches/components/ErrorLaunchesCard.tsx
@@ -0,0 +1,23 @@
+import { CircleAlert, RefreshCcw } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+
+export default function ErrorLaunchesCard() {
+	return (
+		<Card className='flex-1 flex items-center justify-center'>
+			<CardContent className='flex flex-col items-center'>
+				<div className='p-3 bg-red-300 rounded-full mb-2.5 flex items-center justify-center'>
+					<CircleAlert className='text-red-600' />
+				</div>
+
+				<p className='text-red-700'>Erro ao carregar lançamentos</p>
+				<p className='text-red-500 text-center text-xs mb-2'>Tente novamente em alguns instantes.</p>
+				<Button className='bg-red-600 hover:bg-red-500 transition-colors text-white cursor-pointer'>
+					<RefreshCcw />
+					Recarregar
+				</Button>
+			</CardContent>
+		</Card>
+	);
+}
